de3cf206ad9eb5a07d7796b979e79eacd66d243d
Char1sma <github-charisma@32mb.cn>
2016-12-03 22:53:51 +0800

    Detaches configuration from service.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	if ( !defined('BASEPATH')) exit('No Access');
+	define('USER', "root");
+	define('PASS', "");
+	define('DB', "vps");
+	define('KEY', "666");
+	define('PA', "666");//如果设置了密码，只有输入密码后才能看到
+	define('BASEPATH',dirname(dirname(__FILE__)));
+?>
